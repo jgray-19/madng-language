85ffc40f0dbce7578048e67a2e52a0c134eb806a
最萌小汐 <sumneko@hotmail.com>
2022-01-12 19:10:29 +0800

    update builder

--- build/build.ts
+++ build/build.ts
@@ -11,61 +11,13 @@ function readJson(fileName: string) {
     return JSON.parse(text);
 }
 
-function transformGrammarRule(rule: any, propertyNames: string[], transformProperty: (ruleProperty: string) => string) {
-    for (const propertyName of propertyNames) {
-        const value = rule[propertyName];
-        if (typeof value === 'string') {
-            rule[propertyName] = transformProperty(value);
-        }
-    }
-
-    for (var propertyName in rule) {
-        const value = rule[propertyName];
-        if (typeof value === 'object') {
-            transformGrammarRule(value, propertyNames, transformProperty);
-        }
-    }
-}
-
-function transformGrammarRepository(grammar: TmGrammar, propertyNames: string[], transformProperty: (ruleProperty: string) => string) {
-    const repository = grammar.repository;
-    for (let key in repository) {
-        transformGrammarRule(repository[key], propertyNames, transformProperty);
-    }
-}
-
-function getLuaGrammar(getVariables: (luaGrammarVariables: MapLike<string>) => MapLike<string>) {
-    const luaGrammarBeforeTransformation = readJson('lua.tmLanguage.json') as TmGrammar;
-    return updateGrammarVariables(luaGrammarBeforeTransformation, getVariables(luaGrammarBeforeTransformation.variables as MapLike<string>));
-}
-
-function replacePatternVariables(pattern: string, variableReplacers: VariableReplacer[]) {
-    let result = pattern;
-    for (const [variableName, value] of variableReplacers) {
-        result = result.replace(variableName, value);
-    }
-    return result;
-}
-
-type VariableReplacer = [RegExp, string];
-function updateGrammarVariables(grammar: TmGrammar, variables: MapLike<string>) {
-    delete grammar.variables;
-    const variableReplacers: VariableReplacer[] = [];
-    for (const variableName in variables) {
-        // Replace the pattern with earlier variables
-        const pattern = replacePatternVariables(variables[variableName], variableReplacers);
-        variableReplacers.push([new RegExp(`{{${variableName}}}`, "gim"), pattern]);
-    }
-    transformGrammarRepository(
-        grammar,
-        ["begin", "end", "match"],
-        pattern => replacePatternVariables(pattern, variableReplacers)
-    );
-    return grammar;
+function getLuaGrammar() {
+    const luaGrammar = readJson('lua.tmLanguage.json') as TmGrammar;
+    return luaGrammar;
 }
 
 function buildGrammar() {
-    const luaGrammar = getLuaGrammar(grammarVariables => grammarVariables);
+    const luaGrammar = getLuaGrammar();
 
     // Write TypeScript.tmLanguage
     writePlistFile(luaGrammar, 'Syntaxes/Lua.plist');
